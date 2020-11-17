784250c4db6e5ecfaaed808e1890b599c95bf180
arnavkh21 <72095856+arnavkh21@users.noreply.github.com>
2020-11-17 18:20:19 +0300

    Add files via upload

--- kwitter.js
+++ kwitter.js
@@ -4,3 +4,8 @@ user_name= document.getElementById("user_name").value;
 localStorage.setItem("user_name",user_name);
 window.location= "kwitter_room.html";
 }
+function logout(){
+    localStorage.removeItem("user_name");
+    localStorage.removeItem("room_name");
+    window.location = "index.html";
+  }
